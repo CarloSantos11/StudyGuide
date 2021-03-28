23254510047ca55aba0db87bfcd040f92831162d
Steve Simmons <stevenasimmons05@gmail.com>
2021-03-28 10:14:40 -0500

    added get_age file

---
+++ exercises/get_age/main.js
@@ -0,0 +1,13 @@
+// Create age(birthYear) method and make this more dynamic so we can get the current year and we will pass in our birth year and get our age.
+
+// Make use of the Date().
+
+// let getAge = function (birthYear) {
+//     const d = new Date();
+//     console.log(d.getFullYear() - birthYear);
+// }
+
+let getAge = (birthYear) =>
+    console.log(new Date().getFullYear() - birthYear);
+
+getAge(1975);
