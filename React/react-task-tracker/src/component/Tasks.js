bd9943a7517906d9c907d2696b00af36fddf89c8
Carlo Santos <carlo@snapit.solutions>
2021-04-12 04:08:03 -0500

    last class

--- React/react-task-tracker/src/component/Tasks.js
+++ React/react-task-tracker/src/component/Tasks.js
@@ -25,9 +25,7 @@ const tasks = [
 const Tasks = () => {
   return (
     <>
-      {tasks.map((task) => 
-        <h3>{task.text}</h3>
-      )}
+      {tasks.map((task) => <h3>{task.text}</h3>)}
     </>
   )
 }
